542634be98f5e4cb99f789d046818d096c995b0a
IImaxenceII <83035821+IImaxenceII@users.noreply.github.com>
2023-05-11 17:08:05 +0200

    Add files via upload

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,31 @@
-import { run } from 'mocha';
+import { expect } from 'chai';
+import { aggregateUserAccounts } from '../aggregator';
+import { fetchAccounts, fetchTransactions } from '../api';
 
-export function runTests(): void {
-  run();
-}
+describe('API Tests', () => {
+  it('should fetch accounts successfully', async () => {
+    const accounts = await fetchAccounts();
 
-runTests();
+    expect(accounts).to.be.an('array');
+    expect(accounts).to.have.length.above(0);
+  });
+
+  it('should fetch transactions successfully', async () => {
+    const accNumber = '0000001';
+    const transactions = await fetchTransactions(accNumber);
+
+    expect(transactions).to.be.an('array');
+    expect(transactions).to.have.length.above(0);
+  });
+});
+
+
+describe('Aggregator Tests', () => {
+  it('should aggregate user accounts successfully', async () => {
+    const aggregatedAccounts = await aggregateUserAccounts();
+
+    expect(aggregatedAccounts).to.be.an('array');
+    expect(aggregatedAccounts).to.have.length.above(0);
+  });
+});
+
